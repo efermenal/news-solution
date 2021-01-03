a296784708fec7b871e0ed6805461828ff1f9c48
efermenal <ferendher@gmail.com>
2021-01-03 16:00:59 -0300

    1. API key header.

--- app/src/main/java/com/example/news_solution/api/api.kt
+++ app/src/main/java/com/example/news_solution/api/api.kt
@@ -4,28 +4,27 @@ import com.example.news_solution.BuildConfig.API_KEY
 import com.example.news_solution.models.NewsResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface api {
 
+    @Headers("X-Api-Key: $API_KEY")
     @GET("v2/top-headlines")
     suspend fun getBreakingNews(
         @Query("country")
         countryCode : String = "us",
         @Query("page")
-        pageNumber : Int = 1,
-        @Query("apiKey")
-        apiKey : String = API_KEY
+        pageNumber : Int = 1
     ): Response<NewsResponse>
 
+    @Headers("X-Api-Key: $API_KEY")
     @GET("v2/everything")
     suspend fun searchForNews(
         @Query("q")
         searchQuery : String ,
         @Query("page")
-        pageNumber : Int = 1,
-        @Query("apiKey")
-        apiKey : String = API_KEY
+        pageNumber : Int = 1
     ): Response<NewsResponse>
 
 }
